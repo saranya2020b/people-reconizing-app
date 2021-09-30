120b6b01e35006cb33b593ee444bedb14619d420
saranya2020b <81304177+saranya2020b@users.noreply.github.com>
2021-09-30 16:46:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="image_captured" src="'+data_uri+'"/>';
+    });
+};
+
+console.log("ml5 Version: ",ml5.version);
